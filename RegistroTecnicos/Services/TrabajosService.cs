892a0ee7fa1f605780c90ceb1b01277596cf7678
frailyng <frailindes@gmail.com>
2024-09-13 09:08:44 -0400

    Contexto Trabajos

--- RegistroTecnicos/Services/TrabajosService.cs
+++ RegistroTecnicos/Services/TrabajosService.cs
@@ -8,4 +8,25 @@ namespace RegistroTecnicos.Services;
 public class TrabajosService
 {
     private readonly Contexto _context;
+
+
+    public async Task<bool> Guardar(Trabajos Trabajos)
+    {
+        if (!await Existe(Trabajos.TrabajoId))
+            return await Insertar(Trabajos);
+        else
+            return await Modificar(Trabajos);
+    }
+
+    private async Task<bool> Insertar(Trabajos Trabajos)
+    {
+        _context.Trabajos
+        return await _context.SaveChangesAsync() > 0;
+
+    }
+
+    private async Task <bool> Modificar(Trabajos Trabajos)
+    {
+        _context.Update(Trabajos);
+    }
 }
