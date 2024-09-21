e01e6b1ac87a623007a22955008fa06f4dad067d
frailyng <frailindes@gmail.com>
2024-09-20 22:37:53 -0400

    Contexto con namespace

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -1,35 +1,34 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Models;
 
-namespace RegistroTecnicos.DAL
+namespace RegistroTecnicos.DAL;
+
+public class Contexto : DbContext
 {
-    public class Contexto : DbContext
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+    public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Trabajos> Trabajos { get; set; }
+
+    public DbSet<Prioridades> Priidades { get; set; }
+
+    // Configuración de relaciones si es necesario
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        base.OnModelCreating(modelBuilder);
 
-        public DbSet<Tecnicos> Tecnicos { get; set; }
-        public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
-        public DbSet<Clientes> Clientes { get; set; }
-        public DbSet<Trabajos> Trabajos { get; set; }
+        // Configurar relaciones si es necesario
+        modelBuilder.Entity<Trabajos>()
+            .HasOne(t => t.Cliente)
+            .WithMany(c => c.Trabajos)
+            .HasForeignKey(t => t.ClienteId);
 
-        public DbSet<Prioridades> Priidades { get; set; }
-
-        // Configuración de relaciones si es necesario
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Configurar relaciones si es necesario
-            modelBuilder.Entity<Trabajos>()
-                .HasOne(t => t.Cliente)
-                .WithMany(c => c.Trabajos)
-                .HasForeignKey(t => t.ClienteId);
-
-            modelBuilder.Entity<Trabajos>()
-                .HasOne(t => t.Tecnico)
-                .WithMany(te => te.Trabajos)
-                .HasForeignKey(t => t.TecnicoId);
-        }
+        modelBuilder.Entity<Trabajos>()
+            .HasOne(t => t.Tecnico)
+            .WithMany(te => te.Trabajos)
+            .HasForeignKey(t => t.TecnicoId);
     }
 }
 
