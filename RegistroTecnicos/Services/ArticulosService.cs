774da19381d7fabfdf339c5d57ef24319105a807
frailyng <frailindes@gmail.com>
2024-10-18 20:28:34 -0400

    Articulos Service Creado

---
+++ RegistroTecnicos/Services/ArticulosService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services;
+
+public partial class ArticulosService(Contexto contexto)
+{
+    public async Task <List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+    {
+        return await contexto.Articulos
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
