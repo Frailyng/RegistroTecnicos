c4533eb59f940ca579c07f47109ca418e1996102
frailyng <frailindes@gmail.com>
2024-10-18 20:34:52 -0400

    TrabajosDetalle Creado

---
+++ RegistroTecnicos/Models/TrabajosDetalle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class TrabajosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int TrabajoId { get; set; }
+
+    public int ArticuloId { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public double Precio { get; set; }
+
+    public double Costo { get; set; }
+
+    [ForeignKey("TrabajoId")]
+    public virtual Trabajos Trabajo { get; set; } = null!;
+
+}
