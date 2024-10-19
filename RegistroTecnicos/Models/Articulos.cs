2aea911e68d89a6a1f81dbbe00cbfe16cbb5c9a1
frailyng <frailindes@gmail.com>
2024-10-18 20:13:43 -0400

    Modelo Articulos Creado

---
+++ RegistroTecnicos/Models/Articulos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Articulos
+{
+    [Key]
+
+    public int ArticuloId { get; set; }
+
+    [Required(ErrorMessage = "La Descripcion no puede Estar en blanco")]
+
+    public string Descripcion { get; set; }
+
+    public double Costo { get; set; }
+
+    public double Precio { get; set; }
+
+    public int Existencia { get; set; }
+}
