976040a96fa04438c51c1923db0471049df3d7c4
frailyng <frailindes@gmail.com>
2024-10-26 15:38:42 -0400

    Tipos Tecnicos Añadidos Por el Contexto

--- RegistroTecnicos/Services/TiposTecnicosService.cs
+++ RegistroTecnicos/Services/TiposTecnicosService.cs
@@ -7,44 +7,7 @@ using RegistroTecnicos.Models;
 namespace RegistroTecnicos.Services;
 
 public class TiposTecnicosService(IDbContextFactory<Contexto> DbFactory)
-{    // Método adicional para obtener una lista predefinida de tipos
-    public List<TiposTecnicos> ObtenerTiposPredefinidos()
-    {
-        return new List<TiposTecnicos>
-        {
-            new TiposTecnicos
-            {
-                TipoTecnicoId = 1,
-                Descripcion = "Redes",
-                Activo = true
-            },
-            new TiposTecnicos
-            {
-                TipoTecnicoId = 2,
-                Descripcion = "Reparacion",
-                Activo = true
-            },
-            new TiposTecnicos
-            {
-                TipoTecnicoId = 3,
-                Descripcion = "Impresoras",
-                Activo = true
-            },
-            new TiposTecnicos
-            {
-                TipoTecnicoId = 4,
-                Descripcion = "Soporte",
-                Activo = true
-            },
-            new TiposTecnicos
-            {
-                TipoTecnicoId = 5,
-                Descripcion = "Celulares",
-                Activo = false
-            }
-        };
-    }
-   
+{   
     public async Task<bool> Guardar(TiposTecnicos TiposTecnicos)
     {
         if (!await Existe(TiposTecnicos.TipoTecnicoId))
