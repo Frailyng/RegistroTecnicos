2d7ef88c560c009904b0b4fac9b555f8ced0fdd2
frailyng <frailindes@gmail.com>
2024-10-26 21:12:14 -0400

    CotizacionesDetalle Creado

---
+++ RegistroTecnicos/Models/CotizacionesDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class CotizacionesDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        public int CotizacionId { get; set; }
+
+        public int ArticuloId { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public double Precio { get; set; }
+
+        [ForeignKey("CotizacionId")]
+        public virtual Cotizaciones Cotizaciones { get; set; } = null!;
+    }
+}
